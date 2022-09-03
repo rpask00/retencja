ece457b152ef526af56e80abfbfc5632ed5eae11
rpask00 <rp.261000@gmail.com>
2022-09-03 18:02:10 +0200

    lazy loaded modules

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,21 @@
 import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
 
-const routes: Routes = [{
-  ""
-}];
+const routes: Routes = [
+  {
+    path: 'users-list',
+    loadChildren: () => import('./users-list/users-list.module').then(m => m.UsersListModule)
+  },
+  {
+    path: 'users-form',
+    loadChildren: () => import('./users-form/users-form.module').then(m => m.UsersFormModule)
+  },
+  {
+    path: "",
+    redirectTo: "users-list",
+    pathMatch: "full"
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
